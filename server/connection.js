7035915d3f547874ba5b75a542f4e87bcec6ac66
berat-ademi <berat.ademi@gmail.com>
2023-02-19 16:09:53 +0100

    fix .env

--- server/connection.js
+++ server/connection.js
@@ -1,13 +1,11 @@
 const { Client } = require('pg')
-const dotenv = require('dotenv');
-dotenv.config();
+require("dotenv").config();
 
-console.log(`Your port is ${process.env.PORT}`) 
 const client = new Client({
   host: process.env.DB_HOST,
-  user: `${process.env.DB_USER}`,
+  user: process.env.DB_USER,
   port: process.env.DB_PORT,
-  password: `${process.env.DB_PASSWORD}`,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE
 });
 
